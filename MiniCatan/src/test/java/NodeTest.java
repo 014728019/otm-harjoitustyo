27db99adf4d1a33fa909b95e647c3f83fbf15fc0
Jaakko Jokinen <jaakko.a.jokinen@helsinki.fi>
2018-04-13 14:09:20 +0300

    Player test päivitetty

--- MiniCatan/src/test/java/NodeTest.java
+++ MiniCatan/src/test/java/NodeTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -11,34 +6,10 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Jake
- */
 public class NodeTest {
-    
-    public NodeTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
     
     @Before
     public void setUp() {
     }
-    
-    @After
-    public void tearDown() {
-    }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
 }
